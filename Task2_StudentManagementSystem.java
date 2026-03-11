fa038b8f13e5f828e605e627021059cc37d58835
Alok <talok1674@gmail.com>
2026-03-11 21:13:11 +0530

    Implement Student Management System in Java
    Task 2 – Console-Based Student Management System.
    
    This Java program demonstrates a simple Student Management System using a menu-driven console interface. The program allows users to add student details and view the list of students. It uses ArrayList for storing student data and demonstrates basic Object-Oriented Programming concepts in Java.

---
+++ Task2_StudentManagementSystem.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student {
+    int id;
+    String name;
+    int age;
+
+    Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    void display() {
+        System.out.println("ID: " + id + " Name: " + name + " Age: " + age);
+    }
+}
+
+public class Task2_StudentManagementSystem {
+
+    public static void main(String[] args) {
+
+        ArrayList<Student> students = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\n--- Student Management System ---");
+            System.out.println("1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    System.out.print("Enter Student ID: ");
+                    int id = sc.nextInt();
+
+                    sc.nextLine(); 
+                    System.out.print("Enter Student Name: ");
+                    String name = sc.nextLine();
+
+                    System.out.print("Enter Student Age: ");
+                    int age = sc.nextInt();
+
+                    students.add(new Student(id, name, age));
+                    System.out.println("Student added successfully!");
+                    break;
+
+                case 2:
+                    System.out.println("\nStudent List:");
+                    for (Student s : students) {
+                        s.display();
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Exiting program...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice!");
+            }
+
+        } while (choice != 3);
+
+        sc.close();
+    }
+}
